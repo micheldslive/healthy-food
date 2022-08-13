7f4957130639e40f025f4908dca36c5390e745d1
Michel Domingos <michel.silva@penseavanti.com.br>
2022-08-12 21:42:30 -0300

    feat: created footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,24 @@
+import { memo } from 'react'
+import * as C from './styled'
+
+const Footer = () => {
+  return (
+    <C.FooterFull>
+      <C.FooterContainer>
+        <C.FooterContent>
+          <C.Paragraph>Copyrights 2019 Stack. All Rights Reserved.</C.Paragraph>
+          <C.Unlist>
+            <C.List>
+              <C.Anchor>Privacy Policy</C.Anchor>
+            </C.List>
+            <C.List>
+              <C.Anchor>Terms and Conditions</C.Anchor>
+            </C.List>
+          </C.Unlist>
+        </C.FooterContent>
+      </C.FooterContainer>
+    </C.FooterFull>
+  )
+}
+
+export default memo(Footer)
