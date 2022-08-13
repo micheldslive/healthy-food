3f180659926c65892dbd8d63d75572651e9cefe6
Michel Domingos <michel.silva@penseavanti.com.br>
2022-08-12 21:39:04 -0300

    style: creating globalstyles, variables and mixins

---
+++ src/styles/variables.ts
@@ -0,0 +1,23 @@
+import { css } from 'styled-components'
+
+export const Variables = css`
+  :root {
+    // Fonts
+    --font-primary: "Mulish", sans-serif;
+    --font-secondary: "Montserrat", sans-serif;
+    // Colors
+    --white: #ffffff;
+    --grey-1: #fafafc;
+    --grey-2: #dadada;
+    --grey-3: #bcbcbc;
+    --grey-4: #9e9baf;
+    --green-1: #707070;
+    --green-2: #badc58;
+    --blue-1: #2d3561;
+    --blue-2: #1d164d;
+    //Weights
+    --normal: 400;
+    --semiBold: 500;
+    --bold: 700;
+  }
+`
