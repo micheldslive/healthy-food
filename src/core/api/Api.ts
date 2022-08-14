c5f46e533c004868b7a0d0813c26a9d6ef395fe5
Michel Domingos <michel.silva@penseavanti.com.br>
2022-08-14 12:03:39 -0300

    refactor: adjustment in the function of the zip code and favicon path changed.

--- src/core/api/Api.ts
+++ src/core/api/Api.ts
@@ -10,10 +10,9 @@ export const HealthyFoodAPI = () => {
   const AddressingCep = async (cep: string): Promise<IRegister> => {
     const { errorHandler } = new ErrorHandler()
     const [error, response] = await errorHandler(
-      api.get(`${cep}/json`)
+      api.get(`${cep || "00000000"}/json`)
     )
     if (error) throw new Error(error.message)
-    if (response.erro) return response.erro
     return response.data
   }
 
